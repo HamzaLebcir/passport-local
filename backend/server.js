7ce1c0ad344edcc1769715a92b490586e905690c
HamzaLebcir <lebcirhamza7@gmail.com>
2020-12-02 15:42:51 +0100

    first code

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,24 @@ const bodyParser = require("body-parser");
 const app = express();
 const User = require("./user");
 //----------------------------------------- END OF IMPORTS---------------------------------------------------
-mongoose.connect(
-  "mongodb+srv://{Place Your Username Here!}:{Place Your Password Here!}@cluster0-q9g9s.mongodb.net/test?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log("Mongoose Is Connected");
-  }
-);
+
+
+const port = 4000
+const DB_URI = "mongodb://localhost:27017/pasport-local" // mongodb://domain:port/database-name
+// Connect to MongoDB
+mongoose.connect(DB_URI)
+
+// CONNECTION EVENTS
+mongoose.connection.once('connected', function() {
+    console.log("Database connected to " + DB_URI)
+})
+mongoose.connection.on('error', function(err) {
+    console.log("MongoDB connection error: " + err)
+})
+mongoose.connection.once('disconnected', function() {
+    console.log("Database disconnected")
+})
+
 
 // Middleware
 app.use(bodyParser.json());
@@ -46,6 +54,7 @@ require("./passportConfig")(passport);
 
 // Routes
 app.post("/login", (req, res, next) => {
+  console.log(req.body)
   passport.authenticate("local", (err, user, info) => {
     if (err) throw err;
     if (!user) res.send("No User Exists");
@@ -59,6 +68,7 @@ app.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 app.post("/register", (req, res) => {
+  console.log(req.body);
   User.findOne({ username: req.body.username }, async (err, doc) => {
     if (err) throw err;
     if (doc) res.send("User Already Exists");
@@ -79,6 +89,6 @@ app.get("/user", (req, res) => {
 });
 //----------------------------------------- END OF ROUTES---------------------------------------------------
 //Start Server
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log("Server Has Started");
 });
